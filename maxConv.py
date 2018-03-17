827b437387f48292155d498446870639fdc00f9a
Felipe Goncalves Marques <marques.goncalves.felipe@gmail.com>
2018-03-17 15:49:26 +0100

    Refactor improving readability

--- maxConv.py
+++ maxConv.py
@@ -1,22 +1,19 @@
 minus_inf = float("-infinity")
+def convulate(first_function, second_function):
+    conv_function = [minus_inf]*(len(first_function) + len(second_function) - 1)
+    for x_i, f_x_i in enumerate(first_function):
+        for x_j, f_x_j in enumerate(second_function):
+            conv_function[x_i + x_j] = max(conv_function[x_i + x_j], f_x_j + f_x_i)
+    return conv_function
+
 def max_convolution(first_function, *other_functions):
     if (len(other_functions) == 0):
         return first_function
     else:
-        rest_covolution = max_convolution(*other_functions)
-        result_convolution = [minus_inf]*(len(first_function) + len(rest_covolution) - 1)
-        for first in range(len(first_function)):
-            for second in range(len(rest_covolution)):
-                result_convolution[first + second] = max(result_convolution[first + second], first_function[first] + rest_covolution[second])
+        return convulate(first_function, max_convolution(*other_functions))
 
-        return result_convolution
-
-def max_convolution_iterative(*functions):
-    conv_function = functions[0];
-    for fun  in functions[1:]:
-        old_conv_function = conv_function
-        conv_function = [minus_inf]*(len(fun) + len(old_conv_function) - 1)
-        for x_i, f_x_i in enumerate(fun):
-            for x_j, f_x_j in enumerate(old_conv_function):
-                conv_function[x_i + x_j] = max(conv_function[x_i + x_j], f_x_j + f_x_i)
-    return conv_function
+def max_convolution_iterative(first_function, *other_functions):
+    convulated_function = first_function
+    for next_function in other_functions:
+        convulated_function = convulate(next_function, convulated_function)
+    return convulated_function
